a858cb96cb9c3ee42bca07fed7aa1d183d335bc5
LLewis007 <llewis@perscholas.org>
2024-07-12 13:11:15 -0400

    Spring Boot Security UserLogin app using Spring Data Rest implementation. Ms. Lewis

--- src/main/java/com/lewis/userlogin/repository/UserRepository.java
+++ src/main/java/com/lewis/userlogin/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.lewis.userlogin.repository;
 
 import com.lewis.userlogin.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "userdata", path= "userdata")
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
